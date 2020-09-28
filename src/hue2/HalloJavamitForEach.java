2ff21a1540763daff7f077d718feeedb5ab7980f
Teresa <edinger.teresa@sus.htl-grieskirchen.at>
2020-09-28 13:42:24 +0200

    fourth commit

--- src/hue2/HalloJavamitForEach.java
+++ src/hue2/HalloJavamitForEach.java
@@ -34,6 +34,9 @@ public class HalloJavamitForEach {
             System.out.println(list.get(list.indexOf(string)));
         }
 
+        System.out.println("\nlist.forEach((String s) ...)");
+        list.forEach((String s) -> System.out.println(s));
+
     }
 
 }
