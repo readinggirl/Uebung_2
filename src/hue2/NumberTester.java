72a74e239b464a2bc0c28587c03b0628a5cbc6a3
Teresa <edinger.teresa@sus.htl-grieskirchen.at>
2020-09-28 15:54:35 +0200

    setter befuellt und Konstruktor befuellt

--- src/hue2/NumberTester.java
+++ src/hue2/NumberTester.java
@@ -5,30 +5,39 @@
  */
 package hue2;
 
+import java.io.File;
+
 /**
  *
  * @author Teresa
  */
 public class NumberTester {
 
+    private NumberTest oddEvenTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+    private File file;
+
     public static void main(String[] args) {
 
     }
 
     public NumberTester(String fileName) {
-
+        this.file = new File(fileName);
     }
 
     public void setOddEvenTester(NumberTest oddTester) {
+        this.oddEvenTester = oddTester;
 
     }
 
     public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
 
     }
 
     public void setPalindromeTester(NumberTest palindromeTester) {
-
+        this.palindromeTester = palindromeTester;
     }
 
     public void testFile() {
