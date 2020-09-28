52b8352efa04afb7e0b0ad0b87dca0ccf8e234c7
Teresa <edinger.teresa@sus.htl-grieskirchen.at>
2020-09-28 13:38:32 +0200

    second commit

---
+++ src/hue2/HalloJavamitForEach.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hue2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Teresa
+ */
+public class HalloJavamitForEach {
+
+    List<String> list = new ArrayList<>();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HalloJavamitForEach hello = new HalloJavamitForEach();
+        hello.print();
+    }
+
+    public void print() {
+        //random zeugs hinzufuegen
+        for (int i = 0; i < 10; i++) {
+            list.add("1." + i);
+        }
+
+    }
+
+}
