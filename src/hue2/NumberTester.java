68107728def05d6a6d5e8d34531548eca99273a1
Teresa <edinger.teresa@sus.htl-grieskirchen.at>
2020-09-28 15:51:20 +0200

    NumberTester class added and method heads created

---
+++ src/hue2/NumberTester.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hue2;
+
+/**
+ *
+ * @author Teresa
+ */
+public class NumberTester {
+
+    public static void main(String[] args) {
+
+    }
+
+    public NumberTester(String fileName) {
+
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+
+    }
+
+    public void testFile() {
+
+    }
+
+    public interface NumberTest {
+
+        boolean testNumber(int number);
+    }
+
+}
