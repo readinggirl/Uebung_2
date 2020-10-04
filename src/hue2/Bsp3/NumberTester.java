8ebb9d25695b25f02316c932311520eaaa1a7045
Teresa <teresa.edinger@gmail.com>
2020-10-04 21:34:54 +0200

    Beispiel 4 methodheads erstellt

--- src/hue2/Bsp3/NumberTester.java
+++ src/hue2/Bsp3/NumberTester.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hue2;
+package hue2.Bsp3;
 
 import java.io.File;
 import java.io.BufferedReader;
---
+++ src/hue2/Bsp4/Number.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hue2.Bsp4;
+
+/**
+ *
+ * @author Teresa
+ */
+public class Number {
+
+    private double a;
+    private double b;
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+}
